869b7976fe31d74bda0bd47271fa98447f6a2473
Tanya Dubey <dubeytanya2312@gmail.com>
2025-07-06 10:14:01 +0530

    routes setup for Auth

---
+++ week-7/routes/auth.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const auth = require('../middleware/authMiddleware');
+
+// Registering the user
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user)
+      return res.status(400).json({ msg: 'User already exists' });
+
+    user = new User({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.json({ msg: 'User registered successfully' });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+});
+
+// this is login...
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ msg: 'Invalid Credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ msg: 'Invalid Credentials' });
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+});
+
+// Protected Route
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
