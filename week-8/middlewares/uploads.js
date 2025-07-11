fcfb43aa1bba73ecd221bc526c8e982f6b228d82
Tanya Dubey <dubeytanya2312@gmail.com>
2025-07-11 22:36:20 +0530

    uploads added

---
+++ week-8/middlewares/uploads.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|pdf/;
+  const extName = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = allowedTypes.test(file.mimetype);
+
+  if (extName && mimeType) cb(null, true);
+  else cb(new Error('Only images and PDFs allowed'));
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
